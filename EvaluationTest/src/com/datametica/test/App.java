14e276904a3e18c9a51504235d666de2097f3494
sunil0502 <sunil050293@gmail.com>
2015-09-10 10:52:33 +0530

    Running Successfully

--- EvaluationTest/src/com/datametica/test/App.java
+++ EvaluationTest/src/com/datametica/test/App.java
@@ -1,10 +1,27 @@
 package com.datametica.test;
-
+import java.util.Scanner;
 public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			System.out.println("Hi");
+		Scanner sc=new Scanner(System.in);
+		int numLines=sc.nextInt();
+		int count=1;
+		String sign="";
+		for (int i=0;i<numLines;i++)
+		{
+			sign="_";
+		for(int j=numLines-1;j>=0;j--)
+		{
+			if(count>=j)
+			{
+				sign="*";
+			}
+		System.out.print(sign);
+		}
+		count++;
+		System.out.println();
+		}
 	}
 
 }
